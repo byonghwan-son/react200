38ac11d0f98625998fd62f9be1b3d02a3e4f8daf
Byonghwan Son <byonghwan@trial-net.co.kr>
2023-12-08 10:27:46 +0900

    prop 제어하기

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 import "./App.css";
-import PropsBoolean from "./R019_PropsBoolean";
+import PropsNode from "./R023_PropsNode";
+
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <PropsBoolean BooleanTrueFalse={false} />
-      <PropsBoolean BooleanTrueFalse />
+        <PropsNode>
+            <span>node from App.js</span>
+        </PropsNode>
     </div>
   );
 }
