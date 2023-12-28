25a398a7e07d7b8b0706da41644890e35b86201c
byonghwan_son <sonstein@daum.net>
2023-12-28 23:39:41 +0900

    react cookies

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,13 @@
 // import "./App.css"
 // import 'bootstrap/dist/css/bootstrap.css'
 import React from "react"
-import StrAddButton from "./chap03/StrAddButton";
+import R087_cookieRemove from "./chap03/R087_cookieRemove";
 
 function App(props) {
   return (
     <div>
       <h1>Start React 200!</h1>
-      <span>{props.store.getState().data.str}</span><br/>
-      {/*<p>CSS 적용하기</p>*/}
-      <StrAddButton store={props.store} />
+      <R087_cookieRemove />
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import App from "./chap03/react-routers/App";
 import reportWebVitals from "./reportWebVitals";
-import {createStore} from "redux";
-import reducers from "./chap03/reducers";
+import {BrowserRouter} from "react-router-dom";
 
-const store = createStore(reducers)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const listener = () => root.render(<App store={store} />);
-
-store.subscribe(listener)
-listener()
+const listener = () => root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
